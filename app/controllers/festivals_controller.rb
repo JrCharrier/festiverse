2701982fadb9ac74bf456606d89d8b6a9c6cf709
Paul Cabot <paul.claude.cabot@gmail.com>
2025-01-07 11:09:15 +0100

    Adding css

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,5 +1,9 @@
 class FestivalsController < ApplicationController
   def index
-    festivals = Festival.all
+    @festivals = Festival.all
+  end
+
+  def show
+    @festival = Festival.find(params[:id])
   end
 end
