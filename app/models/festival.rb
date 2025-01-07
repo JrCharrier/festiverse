ab7e446226da382363ae882be79b9e84759cfe6d
Emilie Didier <emilie.didier97@gmail.com>
2025-01-07 12:19:43 +0100

    images fix

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,6 +1,7 @@
 class Festival < ApplicationRecord
   has_many :users, through: :favorites
   has_many :users, through: :my_festivals
+  has_one_attached :image
 
   validates :name, :genre, :description, :image_url, :line_up, presence: true
   validates :start_date, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 puts 'Cleaning database...'
 Festival.destroy_all
 
@@ -13,11 +15,17 @@ festival1 = Festival.new(name: 'We Love Green 2025',
                 avant-gardiste, qui traverse les ponts entre indie, électro et urbain. Entre musique, conférences,
                 standup, gastronomie engagée, sport et installations artistiques. Un festival qui attire jusqu'à
                 100 000 personnes et plus de 100 artistes, autour de ses 5 scènes.",
-                image_url: 'app/assets/images/festival1.png',
+                image_url: 'app/assets/images/festivals/festival1.png',
                 line_up: "VENDREDI 6 JUIN : LAYLOW - TIAKOLA - SDM - PAUL KALKBRENNER - YSEULT - KAVINSKY - SUBLIME LIVE & d'autres artistes à venir
                 SAMEDI 7 JUIN : CHARLI XCX - PARCELS - GESAFFELSTEIN - AIR - AMELIE LENS - EZRA COLLECTIVE - MAUREEN - MAGDALENA BAY - HORSEGIIRL - SNOW STRIPPERS - MARCEL DETTMANN - JOLAGREEN23 - THEODORA - SPILL TAB & d'autres artistes à venir
                 DIMANCHE 8 JUIN : LCD SOUNDSYSTEM - FKA TWIGS - BEACH HOUSE - BICEP - CLARA LUCIANI - POLO & PAN - ZAMDANE - DJ KOZE - JUDELINE - AVALON EMERSON - JIMI JULES - LUCKY LOVE - FCUKERS & d'autres artistes à venir"
               )
+
+festival1.image.attach(
+  io: File.open(Rails.root.join('app/assets/images/festivals/festival1.png')),
+  filename: 'festival1.png',
+  content_type: 'image/png'
+)
 
 festival2 = Festival.new(name: 'Le Jardin du Michel 2025',
                 start_date: Date.new(2025, 5, 29),
@@ -30,12 +38,18 @@ festival2 = Festival.new(name: 'Le Jardin du Michel 2025',
                 par des valeurs sociales, culturelles et écologiques afin de promouvoir et dynamiser le territoire du Toulois. En ce sens, tout au long de l’année Le Michel’s
                 Crew développe de nombreux projets : un tremplin musical en faveur des groupes régionaux, le Tremplin du Michel ; un événement destiné au jeune public, Le Jardin des Mômes
                 ; et des soirées concerts, les Offs.",
-                image_url: 'app/assets/images/festival2.png',
+                image_url: 'app/assets/images/festivals/festival2.png',
                 line_up: "JEUDI 29 MAI : FONKY FAMILY & d'autres artistes à venir
                 VENDREDI 30 MAI : JO'AIR - DUB INC - TIKEN JAH FOKOLY & d'autres artistes à venir
                 SAMEDI 31 MAI : POLO & PAN - ULTRA VOMIT - NOVA TWINS & d'autres artistes à venir
                 DIMANCHE 1 JUIN : KYA - TAGADA JONES & d'autres artistes à venir"
               )
+
+festival2.image.attach(
+  io: File.open(Rails.root.join('app/assets/images/festivals/festival2.png')),
+  filename: 'festival2.png',
+  content_type: 'image/png'
+)
 
 festival3 = Festival.new(name: 'Festival Marsatac - 27ème Édition',
                 start_date: Date.new(2025, 6, 13),
@@ -47,9 +61,15 @@ festival3 = Festival.new(name: 'Festival Marsatac - 27ème Édition',
                 L'accès aux mineurs non accompagnés est strictement interdit ! Le festival Marsatac sera accessible aux mineurs à partir de 12 ans uniquement accompagnés d'un majeur
                 responsable, désigné par le tuteur légal du mineur par décharge. Une décharge est valable uniquement pour deux mineurs. Un contrôle d'identité sera effectué à l'entrée du site.
                 Il conviendra de venir le jour du festival avec la décharge dédiée, accompagnée d'une photocopie de la carte d'identité du tuteur légal, du majeur responsable et du mineur concerné.",
-                image_url: 'app/assets/images/festival3.png',
+                image_url: 'app/assets/images/festivals/festival3.png',
                 line_up: "LAYLOW - FONKY FAMILY - BRUTALISMUS 3000 & d'autres artistes à venir"
               )
+
+festival3.image.attach(
+  io: File.open(Rails.root.join('app/assets/images/festivals/festival3.png')),
+  filename: 'festival3.png',
+  content_type: 'image/png'
+)
 
 festival1.save!
 festival2.save!
