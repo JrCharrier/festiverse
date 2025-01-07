833028a01228709734f6b7c23e925ee6ac387674
Emilie Didier <emilie.didier97@gmail.com>
2025-01-07 17:18:03 +0100

    navbar et searchbar

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,6 +1,11 @@
 class FestivalsController < ApplicationController
   def index
     @festivals = Festival.all
+    if params[:query].present?
+      @festivals = Festival.search_by_name_genre_start_date_line_up_and_address(params[:query])
+    else
+      @festivals = Festival.all
+    end
   end
 
   def show
--- app/models/festival.rb
+++ app/models/festival.rb
@@ -6,4 +6,9 @@ class Festival < ApplicationRecord
   validates :name, :genre, :description, :image_url, :line_up, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true, comparison: { greater_than: :start_date }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_genre_start_date_line_up_and_address, against: [ :name, :genre, :address, :start_date, :line_up ], using: {
+    tsearch: { prefix: true }
+  }
 end
