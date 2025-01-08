544ba5e9ec0cba8ecdfefa7e10e7a22b60a5b7e6
Emilie Didier <emilie.didier97@gmail.com>
2025-01-08 18:26:01 +0100

    ajout catégories recherche

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,7 +1,9 @@
 class FestivalsController < ApplicationController
   def index
-    @festivals = Festival.all
-    if params[:query].present?
+    if params[:genres].present?
+      @genre = params[:genres].upcase
+      @festivals = Festival.where("'#{@genre}' = ANY (genres)")
+    elsif params[:query].present?
       @festivals = Festival.search_by_name_genre_start_date_line_up_and_address(params[:query])
     else
       @festivals = Festival.all
@@ -10,5 +12,12 @@ class FestivalsController < ApplicationController
 
   def show
     @festival = Festival.find(params[:id])
+  end
+
+  def category
+    @genre = params[:genre].downcase
+    @festivals = Festival.where("? = ANY (genres)", @genre)
+
+    render :index
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_08_105408) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_08_164538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,7 +64,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_08_105408) do
     t.string "name"
     t.date "start_date"
     t.date "end_date"
-    t.string "genre", default: [], array: true
     t.text "description"
     t.string "image_url"
     t.text "line_up"
